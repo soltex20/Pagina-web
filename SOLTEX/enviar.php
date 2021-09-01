cdfd7858b0a6962bc6856f50be4d16200ef20817
soltex20 <soltex2021@hotmail.com>
2021-08-31 21:52:13 -0500

    cambios para el correo

--- SOLTEX/enviar.php
+++ SOLTEX/enviar.php
@@ -10,6 +10,27 @@ if (isset($_POST['correo']) && isset($_POST['mensaje']) ) {
     $headers = "From:" . $from;
 
     if (email($from, $to, $subject,$message, $headers)) {
+        $contact = new PHP_Email_Form;
+  $contact->ajax = true;
+  
+  $contact->to = $to;
+  $contact->from_name = "ejemplo";
+  $contact->from_email = $from;
+  $contact->subject = $subject;
+
+  // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
+  
+  $contact->smtp = array(
+    'host' => 'smtp.live.com',
+    'username' => 'soltex2021@hotmail.com',
+    'password' => 'soltex20',
+    'port' => '587'
+  );
+  
+
+  $contact->add_message($mensaje);
+
+  echo $contact->send();
         echo 1;
     } else {
         echo 0;
@@ -17,6 +38,7 @@ if (isset($_POST['correo']) && isset($_POST['mensaje']) ) {
 } else {
     echo 0;
 }
+
 
 
 ?>
