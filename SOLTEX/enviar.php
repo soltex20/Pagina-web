f6e46a4f53a63f8dd270b71d276a35aa8dcfb8eb
José Peña <lsca94@hotmail.com>
2021-09-03 17:43:38 -0500

    cambios email

--- SOLTEX/enviar.php
+++ SOLTEX/enviar.php
@@ -1,44 +1,15 @@
 <?php
-if (isset($_POST['correo']) && isset($_POST['mensaje']) ) {
+
     $correo = $_POST['correo'];
     $mensaje = $_POST['mensaje'];
-
-    $from = $correo;
+    $from = "soltexin@soltex-innovation.axwebsite.com";
     $to = "soltex2021@hotmail.com";
     $subject = "Nuevo mensaje de: ". $correo;
     $message = $mensaje;
-    $headers = "From:" . $from;
+    $headers = "Mensaje proveniente del sitio Web: "; 
 
-    if (email($from, $to, $subject,$message, $headers)) {
-        $contact = new PHP_Email_Form;
-  $contact->ajax = true;
-  
-  $contact->to = $to;
-  $contact->from_name = "ejemplo";
-  $contact->from_email = $from;
-  $contact->subject = $subject;
+    mail($to,$subject,$message,$headers);
 
-  // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
-  
-  $contact->smtp = array(
-    'host' => 'smtp-mail.outlook.com',
-    'username' => 'soltex2021@hotmail.com',
-    'password' => 'soltex20',
-    'port' => '587'
-  );
-  
-
-  $contact->add_message($mensaje);
-
-  echo $contact->send();
-        echo 1;
-    } else {
-        echo 0;
-    }
-} else {
-    echo 0;
-}
-
-
-
+    echo "<script>alert('Correo Enviado con Éxito')</script>";
+    echo "<script>setTimeout(\"location.href='index.html'\",1000)</script>";
 ?>
